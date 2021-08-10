43e25e22df4eefb1caa374c67794f03f5ae2c4bc
xiongfeng <343138759@qq.com>
2021-08-10 20:16:28 +0800

    封装面包屑组件

---
+++ src/components/library/xtx-bread.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="xtx-bread">
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XtxBread',
+  props: {
+    parentName: {
+      type: String,
+      default: ''
+    },
+    parentPath: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style lang='scss'>
+.xtx-bread{
+  display: flex;
+  padding: 25px 10px;
+  &-item {
+    a {
+      color: #666;
+      transition: all .4s;
+      &:hover {
+        color: $xtxColor;
+      }
+    }
+    &:last-child{
+        .iconfont{
+            display: none;
+        }
+    }
+  }
+  i {
+    font-size: 12px;
+    margin-left: 5px;
+    margin-right: 5px;
+    line-height: 22px;
+  }
+}
+</style>
