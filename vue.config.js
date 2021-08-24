701f7342c718b445d562588508dd735110cdce41
xiongfeng <343138759@qq.com>
2021-08-24 10:23:25 +0800

    开启域名访问权限

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,7 @@ module.exports = {
       .use('url-loader')
       .loader('url-loader')
       .tap(options => Object.assign(options, { limit: 10000 }))
+    // 这个是给webpack-dev-server开启可IP和域名访问权限。
+    config.devServer.disableHostCheck(true)
   }
 }
