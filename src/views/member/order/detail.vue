57cd185bb0a2ca0efed4dea7a4f18a5147d42c98
xiongfeng <343138759@qq.com>
2021-09-07 09:50:09 +0800

    完成个人中心进度条渲染

--- src/views/member/order/detail.vue
+++ src/views/member/order/detail.vue
@@ -3,6 +3,14 @@
     <!-- 操作栏 -->
     <DetailAction :order="order" />
     <!-- 步骤条 组件xtx-steps.vue-->
+       <!-- 步骤条-->
+    <XtxSteps :active='order.orderState===6?0:order.orderState'>
+      <XtxStepsItem title="提交订单" :desc="order.createTime" />
+      <XtxStepsItem title="付款成功"  :desc="order.payTime?order.payTime:''"/>
+      <XtxStepsItem title="商品发货" :desc="order.consignTime?order.consingTime:''" />
+      <XtxStepsItem title="确认收货" :desc="order.endTime?order.endTime:''"/>
+      <XtxStepsItem title="订单完成" :desc="order.closeTime?order.closeTime:''"/>
+    </XtxSteps>
     <!-- 物流栏 -->
     <!-- 订单商品信息 -->
   </div>
